c365e1cf3063e063f7a8ddf1ae018b70f4123481
Andre Neves de Amorim <andre.php6@gmail.com>
2020-03-22 10:57:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,28 +3,13 @@ var fs = require('fs')
 app = express();
 
 // read in the APP_MSG env var
-var msg = process.env.APP_MSG;
+var msg = process.env.texto;
 
 var response;
 
 app.get('/', function (req, res) {
 
-    response = 'Value in the APP_MSG env var is => ' + msg + '\n';
-
-    // Read in the secret file
-    fs.readFile('/opt/app-root/secure/myapp.sec', 'utf8', function (secerr,secdata) {
-        if (secerr) {
-            console.log(secerr + '\n');
-            response += secerr + '\n';
-        }
-        else {
-            response += 'The secret is => ' + secdata + '\n';
-        }
-
-        //send the response to the client
-        res.send(response);
-    });
-
+    response = 'Value in the texto env var is => ' + msg + '\n';
 });
 
 app.listen(8080, function () {
